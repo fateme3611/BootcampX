f394ca9f9d8f00267b19e365592543b0374393d6
fateme <fateme3611@yahoo.com>
2021-12-10 19:00:20 -0500

    parametrize the teacher and student database

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'vagrant',
-    password: '',
-    host: '192.168.20.200',
-    database: 'bootcampx'
+  user: 'vagrant',
+  password: '',
+  host: '192.168.20.200',
+  database: 'bootcampx'
 });
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
@@ -15,8 +15,13 @@ JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
 ORDER BY teacher;
 `)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [cohortName];
+
+pool.query(queryString, values)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    })
+  });
